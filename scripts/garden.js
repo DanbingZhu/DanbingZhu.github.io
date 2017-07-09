09649e9ca3e000b81cb48817ae6b85d1a758ae29
Daniel <eggpie@sjtu.edu.cn>
2017-07-09 00:06:00 -0700

    slider modified

--- scripts/garden.js
+++ scripts/garden.js
@@ -1,3 +1,5 @@
+'use strict';
+
 $(document).ready(function() {
     $('#tomato-btn').on('click',function() {
         $('#tomato-content').fadeToggle();
@@ -17,8 +19,8 @@ $(document).ready(function() {
 });
 
 $(function() {
-	var width = 720;
-	var animationSpeed = 1000;
+	var width = 400;
+	var animationSpeed = 2000;
 	var pause = 3000;
 	var currentSlide = 1;
 
@@ -27,13 +29,25 @@ $(function() {
 	var $slideContainer = $slider.find('.slides');
 	var $slides = $slideContainer.find('.slide');
 
-	setInterval(function(){
-		$slideContainer.animate({'margin-left': '-='+width}, animationSpeed, function() {
-			currentSlide++;
-			if (currentSlide === $slides.length) {
-				currentSlide = 1;
-				$slideContainer.css('margin-left', 0);
-			}
-		});
-	}, pause)
+	var interval;
+
+	function startSlider(){
+		interval = setInterval(function() {
+			$slideContainer.animate({'margin-left': '-='+width}, animationSpeed, function() {
+				currentSlide++;
+				if (currentSlide === $slides.length) {
+					currentSlide = 1;
+					$slideContainer.css('margin-left', 0);
+				}
+			});
+		}, pause);
+	}
+
+	function stopSlider(){
+		clearInterval(interval);
+	}
+
+	$slider.on('mouseenter', stopSlider).on('mouseleave', startSlider);
+
+	startSlider();
 });
